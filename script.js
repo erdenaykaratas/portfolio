56d16387ee7ae3d3aa356c0ccbc1196fa3d31891
Erdenay Karataş <erdenay.karatas@icloud.com>
2023-12-30 22:14:42 +0000

    still testing

--- script.js
+++ script.js
@@ -15,3 +15,25 @@ function toggleMenu() {
       scrollPopup.classList.remove("show");
     }
   });
+
+  window.addEventListener("scroll", () => {
+    const scrollPopup = document.createElement("div");
+    scrollPopup.className = "scroll-popup";
+    scrollPopup.innerHTML = "Hello! Check out my awesome portfolio!";
+    
+    const scrollThresholds = [300, 600, 900]; // Adjust these values based on your preference
+    
+    scrollThresholds.forEach((threshold, index) => {
+      if (window.scrollY > threshold && !scrollPopup.classList.contains(`show-${index}`)) {
+        const randomPosition = Math.floor(Math.random() * (window.innerWidth - 200)); // Adjust 200 based on the pop-up's width
+        scrollPopup.style.top = `${threshold}px`;
+        scrollPopup.style.left = `${randomPosition}px`;
+        scrollPopup.classList.add(`show-${index}`);
+        document.body.appendChild(scrollPopup.cloneNode(true));
+
+        setTimeout(() => {
+          scrollPopup.classList.remove(`show-${index}`);
+        }, 2000); // Adjust the time the pop-up stays visible
+      }
+    });
+  });
