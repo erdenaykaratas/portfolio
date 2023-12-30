3167fd7126d4756f66e3e9ba216666146f4b18c5
Erdenay Karataş <erdenay.karatas@icloud.com>
2023-12-30 22:08:44 +0000

    Some testing

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  window.addEventListener("scroll", () => {
+    const scrollPopup = document.querySelector(".scroll-popup");
+    const scrollThreshold = 300; // Adjust this value based on your preference
+
+    if (window.scrollY > scrollThreshold) {
+      scrollPopup.classList.add("show");
+    } else {
+      scrollPopup.classList.remove("show");
+    }
+  });
